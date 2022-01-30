0146749261e6dc320fd3a6d676112e29bac52fc5
r_boo <r_boo@DESKTOP-61FSOOO.attlocal.net>
2022-01-29 17:59:03 -0800

    Finish Robot Race.

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -1,36 +1,65 @@
 package _00_Intro_To_Arrays;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+String[] strings = new String[5];
+strings[0] = "one";
+strings[1] = "two";
+strings[2] = "three";
+strings[3] = "four";
+strings[4] = "five";
         // 2. print the third element in the array
-
+System.out.println(strings[2]);
         // 3. set the third element to a different value
-
+strings[2] = "six";
         // 4. print the third element again
-
+System.out.println(strings[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+for (int i = 0; i < strings.length; i++) {
+	strings[i] = "Haha!";
+}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+for (int i = 0; i < strings.length; i++) {
+	System.out.println(strings[i]);
+}
         // 7. make an array of 50 integers
-
+int[] ints = new int[50];
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+Random ran = new Random();
+int ranInt = 0;
+for (int i = 0; i < ints.length; i++) {
+	ranInt = ran.nextInt(9999)+1;
+	ints[i] = ranInt;
+}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+int smallest = 999999999;
+for (int i = 0; i < ints.length; i++) {
+	if(ints[i]<smallest) {
+		smallest = ints[i];
+	}
+}
+System.out.println(smallest);
         // 10 print the entire array to see if step 8 was correct
-
+for (int i = 0; i < ints.length; i++) {
+	System.out.println(ints[i]);
+}
         // 11. print the largest number in the array.
-
+int largest = 0;
+for (int i = 0; i < ints.length; i++) {
+	if(ints[i]>largest) {
+		largest = ints[i];
+	}
+}
+System.out.println(largest);
         // 12. print only the last element in the array
-
+System.out.println(ints[ints.length-1]);
     }
 }
--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -2,29 +2,73 @@ package _00_Intro_To_Arrays;
 
 import java.util.Random;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+import java.awt.Color;
+import java.awt.Graphics;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
     // 1. make a main method
+	static Robot[] robs = new Robot[30];
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	static boolean isRacing = true;
+
+	
+public static void main(String[] args) {
+	int lineDifference = 10;
+	for(int i=0; i < robs.length; i++) {
+		robs[i] = new Robot(1890,1078);
+		robs[i].setSpeed(60);
+		robs[i].show();
+		robs[i].miniaturize();
+		lineDifference = lineDifference+15;
+	}
+robs[0].setWindowSize(1920, 1080);
+	while(isRacing) {
+	for (int i = 0; i < robs.length; i++) {
+	Random ran = new Random();
+	int ranInt = ran.nextInt(49)+1;
+	 robs[i].move(ranInt);
+	 if(robs[i].getY() <= 20) {
+		// isRacing=false;
+		// JOptionPane.showMessageDialog(null, "Robot #"+ (i+1) + " has won the race! Give them a round of applause!");
+		// break;
+		 robs[i].turn(-90);
+		 robs[i].setY(21);
+	 }
+	 if(robs[i].getX() <= 20) {
+		 robs[i].turn(-90);
+		 robs[i].setX(21);
+	 }
+	 if(robs[i].getY() >= 1080) {
+		 robs[i].turn(-90);
+		 robs[i].setY(1078);
+	 }
+}
+}
+}
 
         // 2. create an array of 5 robots.
-
+//done
         // 3. use a for loop to initialize the robots.
-
+//done
         // 4. make each robot start at the bottom of the screen, side by side, facing up
-    
+    //done
         // 5. use another for loop to iterate through the array and make each robot move
         // a random amount less than 50.
-    
+    //done
         // 6. use a while loop to repeat step 5 until a robot has reached the top of the
         // screen.
-    
+    //done
         // 7. declare that robot the winner and throw it a party!
-    
+    //done
         // 8. try different races with different amounts of robots.
-    
+    //done
         // 9. make the robots race around a circular track.
 
 }
