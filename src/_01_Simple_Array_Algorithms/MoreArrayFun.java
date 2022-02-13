7d1aecf9e67965056c4305272b8443b9707e63a9
r_boo <r_boo@192.168.1.101>
2022-02-12 17:54:45 -0800

    Start fireworks

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -1,39 +1,74 @@
 package _01_Simple_Array_Algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MoreArrayFun {
-    //1. Create a main method to test the other methods you write.
-public static void main(String[] args) {
-	printDemString(2);
-}
+	// 1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
+		String[] base = new String[12];
+		for (int i = 0; i < base.length; i++) {
+			base[i] = "Base "+ (i+1);
+		}
+		System.out.println("------------------");
+		printDemString(base);
+		System.out.println("------------------");
+		printStringDem(base);
+		System.out.println("------------------");
+		printDmtig(base);
+		System.out.println("------------------");
+		printRmetdsIgn(base);		
+		System.out.println("------------------");
 
-
-    static //2. Write a method that takes an array of Strings and prints all the Strings in the array.
-void printDemString(int numberOfDemString) {
-	String[] demString = new String[numberOfDemString];
-	for (int i = 0; i < demString.length; i++) {
-		demString[i] = "DemString";
-		System.out.println(demString[i]);
 	}
-}
 
-
-    //3. Write a method that takes an array of Strings and prints all the Strings in the array
-    //   in reverse order.
-static void printStringDem(int numberOfStringDem) {
-	String[] stringDem = new String[numberOfStringDem];
-	for (int i = 0; i < stringDem.length; i++) {
-		
+	static // 2. Write a method that takes an array of Strings and prints all the Strings
+			// in the array.
+	void printDemString(String[] demString) {
+		for (int i = 0; i < demString.length; i++) {
+			//demString[i] = "DemString " + (i + 1);
+			System.out.println(demString[i]);
+		}
 	}
-}
 
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	static void printStringDem(String[] stringDem) {
+		for (int i = stringDem.length; i > 0; i--) {
+			//stringDem[i - 1] = "StringDem " + (i);
+			System.out.println(stringDem[i - 1]);
+		}
+	}
 
-    //4. Write a method that takes an array of Strings and prints every other String in the array.
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	static void printDmtig(String[] dmtig) {
+		for (int i = 0; i < dmtig.length; i++) {
+			//dmtig[i] = "Dmtig " + (i + 1);
+			if (0 == i % 2) {
+				System.out.println(dmtig[i]);
+			}
+		}
+	}
 
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+ static void printRmetdsIgn(String[] rmetdsIgn) {
+	ArrayList<String> rmetdsIgnS = new ArrayList<String>(Arrays.asList(rmetdsIgn));
+	 Random ran = new Random();
+	 int ranInt = 0;
+		// rmetdsIgn[i] = "RmetdsIgn "+ (i+1);
+	 int sizes = rmetdsIgnS.size();
+	 for (int i = 0; i < sizes; i++) {
+		 ranInt = ran.nextInt(rmetdsIgnS.size());
+		 System.out.println(rmetdsIgnS.get(ranInt));
+		 rmetdsIgnS.remove(ranInt);
+	}
 
-    //5. Write a method that takes an array of Strings and prints all the Strings in the array
-    //   in a completely random order. Almost every run of the program should result in a different order.
-
-
+ }
+ 
 }
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -1,46 +1,51 @@
 package _01_Simple_Array_Algorithms;
 
+import java.util.Iterator;
+
 public class _00_1D_Array_Methods {
-    //1. Complete the method so that it returns the sum of all
-    //   of the integers in the array being passed in
-    public static int sumIntArray(int[] values) {
-int sum = 0;
-for (int i = 0; i < values.length; i++) {
-	sum = sum + values[i];
-}
-        return sum;
-    }
-
-    //2. Complete the method so that it returns the average of all
-    //   of the integers in the array being passed in
-    public static double averageIntArray(int[] values) {
-double total = 0.0;
-for (int i = 0; i < values.length; i++) {
-	total = total + values[i];
-}
-        return total/values.length;
-    }
-
-
-    //3. Complete the method so that it returns true if the integer
-    //   array contains the value specified by the second parameter.
-    //   It should otherwise return false.
-    public static boolean containsIntValue(int[] array, int value) {
-
-        return false;
-    }
-
-    //4. Complete the method so that it returns the index of the,
-    //   first instance that the specified value occurs in the array.
-    //   If the array does not contain the specified value, it should return -1.
-    public static int getIndex(int[] arr, int value) {
-int specif = -1;
-    	for (int i = 0; i < arr.length; i++) {
-	if(arr[i]==value) {
-		specif = arr[i];
-				break;
+	// 1. Complete the method so that it returns the sum of all
+	// of the integers in the array being passed in
+	public static int sumIntArray(int[] values) {
+		int sum = 0;
+		for (int i = 0; i < values.length; i++) {
+			sum = sum + values[i];
+		}
+		return sum;
 	}
-}
-        return specif;
-    }
+
+	// 2. Complete the method so that it returns the average of all
+	// of the integers in the array being passed in
+	public static double averageIntArray(int[] values) {
+		double total = 0.0;
+		for (int i = 0; i < values.length; i++) {
+			total = total + values[i];
+		}
+		return total / values.length;
+	}
+
+	// 3. Complete the method so that it returns true if the integer
+	// array contains the value specified by the second parameter.
+	// It should otherwise return false.
+	public static boolean containsIntValue(int[] array, int value) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// 4. Complete the method so that it returns the index of the,
+	// first instance that the specified value occurs in the array.
+	// If the array does not contain the specified value, it should return -1.
+	public static int getIndex(int[] arr, int value) {
+		int specif = -1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == value) {
+				specif = i;
+				break;
+			}
+		}
+		return specif;
+	}
 }
